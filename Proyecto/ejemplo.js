6469374809138c613392fe6514a839f02dc05fa7
Renelemus097 <lm0298032023@unab.edu.sv>
2023-10-30 15:33:29 -0600

    cambios

---
+++ Proyecto/ejemplo.js
@@ -0,0 +1,23 @@
+function cargarInicio(){
+    let canvas = document.getElementById("canvas")
+if(canvas && canvas.getContext){
+    let ctx = canvas.getContext("2d")
+    if(ctx){
+        ctx.fillStyle = "yellow";
+            ctx.strokeStyle = "#ff0000";
+
+            ctx.lineWidth = 5;
+            //x,y ancho y alto
+            ctx.fillRect(50,50,100,100)
+            // Metodo parea dibujar lineas x,y, w , h
+            ctx.strokeRect(50,50,100,100)
+
+
+            ctx.fillStyle = "rgba(250,250,0,0.25)";
+            ctx.fillRect(100,50,100,100)
+            ctx.strokeRect(50,50,100,100)
+    }
+}
+}
+
+window.onload = cargarInicio()
